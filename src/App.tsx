9c152fabca9f4cecacf2515ae2e2ce62d7096d3f
Adriano Moreira <adr_0603@yahoo.com.br>
2021-12-30 20:24:20 -0300

    sidebar works

--- src/App.tsx
+++ src/App.tsx
@@ -60,26 +60,10 @@ export function App() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-    <SideBar />
-    <Content />
+      <SideBar />
+      <Content />
     </div>
-    // <div style={{ display: 'flex', flexDirection: 'row' }}>
-    //   <nav className="sidebar">
-    //     <span>Watch<p>Me</p></span>
 
-    //     <div className="buttons-container">
-    //       {genres.map(genre => (
-    //         <Button
-    //           key={String(genre.id)}
-    //           title={genre.title}
-    //           iconName={genre.name}
-    //           onClick={() => handleClickButton(genre.id)}
-    //           selected={selectedGenreId === genre.id}
-    //         />
-    //       ))}
-    //     </div>
-
-    //   </nav>
 
     //   <div className="container">
     //     <header>
