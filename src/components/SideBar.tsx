677bb84260ec9b1488c6f801d86df42946d880f3
Adriano Moreira <adr_0603@yahoo.com.br>
2021-12-30 20:02:49 -0300

    Mounting structure

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,18 @@
+import { useState, useEffect } from "react";
+
+import '../styles/global.scss';
+import '../styles/sidebar.scss';
+
+import { api } from '../services/api';
+
+import { Button } from './Button';
+
 export function SideBar() {
   // Complete aqui
+  return(
+
+      <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+      </nav>
+  )
 }
